d86c8306d723c7a6ac9e536471ac17156db54f86
Bikram Mondal <codesnippets45@gmail.com>
2025-05-18 03:22:23 +0530

    Create reverseArray.c

---
+++ reverseArray.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("Enter the number of elements present in the array: ");
+    scanf("%d",&num);
+    int arr[num];
+
+    for(int i=0; i<num; i++){
+        printf("Enter the %d element: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("Your original array is: \n");
+    for(int i=0; i<num; i++){
+        
+        printf("%d\n",arr[i]);
+    }
+
+    for(int i=0; i<num/2; i++){
+        int firstValue = arr[i];
+        int secondValue = arr[num-i-1];
+        arr[i] = secondValue;
+        arr[num-i-1] = firstValue;
+    }
+    printf("Your reversed array is: \n");
+    for(int i=0; i<num; i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
