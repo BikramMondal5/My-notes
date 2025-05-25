6a285c0d85dbc4ed13fc8cc2e5cd14da9e9d9035
BikramMondal5 <codesnippets45@gamil.com>
2025-05-26 00:10:13 +0530

    perfect number

---
+++ c programming/perfectNumber.c
@@ -0,0 +1,33 @@
+// To check whether the number given by the user is perfect number or not. A perfect number is a positive number in which sum of all positive divisors excluding that number is equal to that number. For example, 6 is a perfect number because its divisors are 1, 2, and 3, and their sum is 6 (1 + 2 + 3 = 6)
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    // Get the number from the user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check if the number is positive
+    if (num <= 0) {
+        printf("Error: The number must be positive.\n");
+        return 1;
+    }
+
+    // Find the sum of all positive divisors excluding the number itself
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    // Check if the sum is equal to the number
+    if (sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
+
+    return 0;
+}
