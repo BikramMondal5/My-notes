ec7d68ef44f52a800e818b70e0263a8f191e989e
Bikram Mondal <codesnippets45@gmail.com>
2025-07-21 12:39:03 +0530

    directories create2

---
+++ Python notes/experiment.py
@@ -0,0 +1,7 @@
+name = "Bikram Mondal"
+roll = 12
+cgpa = 9.8
+
+print(type(name))
+print(type(cgpa))
+print(type(roll))
---
+++ c programming/experiment.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    char name[50] = "Bikram Mondal";
+    printf("%s",name);
+
+    return 0;
+}
