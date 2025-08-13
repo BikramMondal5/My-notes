d99528fd9e8c217513d5c0e98de801f76f32509b
Bikram Mondal <codesnippets45@gmail.com>
2025-08-14 01:48:09 +0530

    new notes

---
+++ Python notes/classWork/main.py
@@ -0,0 +1,4 @@
+## Access modifiers are of three types: 1. Private, Protected and Public
+## To access private variables, we use __<variable-name>
+## Four pillars of OOP are: 1. Encapsulation 2. Abstraction 3. Inheritance 4. Polymorphism
+ 
---
+++ R Programming/topper.R
@@ -0,0 +1,8 @@
+# Create a vector of student names
+students = c("Bikram", "Koushik", "Arijit", "Aarav", "Goutam")
+
+# Randomly select a topper
+toppers = sample(students, 1)
+
+# Display the topper
+cat("🏆 The topper of the class is:", toppers, "\n")
