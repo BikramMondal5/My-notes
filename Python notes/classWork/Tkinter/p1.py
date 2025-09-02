1b067789d0f581179a6349ba9f74807e5d24a949
Bikram Mondal <codesnippets45@gmail.com>
2025-09-02 14:44:12 +0530

    python lab8

---
+++ Python notes/classWork/Tkinter/p1.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Create the main application window
+root = tk.Tk()
+root.title("My First Tkinter App")
+
+# Set window size
+root.geometry("400x300")
+
+# Start the event loop
+root.mainloop()
